be87f807d55bd948ef91abeb782ad3987d6c047b
mmadhuparna <mmadhuparna@yahoo.com>
2018-09-07 19:17:38 +0530

    commit

---
+++ myscript.js
@@ -0,0 +1,15 @@
+//creating function map
+function map(array, callback) {
+    var result = []; // Create a new Array
+    for (var i = 0; i < array.length; i++){
+      result[i] = callback(array[i]);
+    }
+     return result;
+}
+ //calling function map in variable numbers
+var numbers = map([2, 4, 6], function(num){
+       return num*2;
+});
+console.log(numbers);
+
+  
